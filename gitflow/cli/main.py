64be9e35f4d79e49699a016b6132feb719dd995d
Federico Bitondo <federico.bitondo@egluetechnologies.com>
2023-10-18 13:06:32 +0200

    first working program

--- gitflow/cli/main.py
+++ gitflow/cli/main.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+from typing import Optional
+
 import typer
+from git import Repo
 from typer import Option
 from typing_extensions import Annotated
 
 import gitflow
 
+repo = Repo(Path.cwd(), search_parent_directories=True)
 app = typer.Typer(no_args_is_help=True, pretty_exceptions_enable=False, add_completion=False)
-# Add functions from other files like:
-# app.command()(my_subcommand)
 
 
 def version_callback(value: bool):
@@ -30,6 +33,38 @@ def common(
     ] = False,
 ):
     pass
+
+
+def issue_id(id: str):
+    return f"issue-{id}" if id else "no-ref"
+
+
+@app.command()
+def feature(
+    name: Annotated[str, typer.Option(prompt=True)],
+    id: Annotated[Optional[str], typer.Argument(callback=issue_id)] = None,
+):
+    branch = f"feature/{id}/{name.replace(' ', '-')}"
+    repo.git.checkout(branch, b=True)
+    print(f"created branch {branch}")
+
+
+@app.command()
+def bug(
+    name: Annotated[str, typer.Option(prompt=True)],
+    id: Annotated[Optional[str], typer.Argument(callback=issue_id)] = None,
+):
+    branch = f"bugfix/{id}/{name.replace(' ', '-')}"
+    repo.git.checkout(branch, b=True)
+
+
+@app.command()
+def complete(target_branch: Annotated[str, typer.Argument()] = "main"):
+    purged_branch = repo.active_branch
+    repo.git.checkout(target_branch)
+    repo.git.pull()
+    repo.git.branch("-D", purged_branch.name)
+    print(f"removed branch {purged_branch.name}, currently on {target_branch}")
 
 
 def run_app():
